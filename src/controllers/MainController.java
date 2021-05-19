c91d25a3a88e3eb09647bbddf3a87024855d5db7
Mitchell Mark-George <mitchellmmarkgeorge@gmail.com>
2021-05-19 21:54:54 +0100

    documenting controllers

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,18 +1,18 @@
 /**
- * The controller class for the main stage of the application. T
- * his stage has the menu, the ListView for projects, and a TableView to display the current projects versions. 
- * It also implements various methods that are triggered by buttons and menu items.
+ * The controller class for the main stage of the application. 
+ * This stage has the menu, the ListView for projects, and a TableView to display the current projects versions. 
+ * It also implements various methods that are triggered by buttons and menu items. It implements the Controller interface and its methods.
  * 
  * @author Mitchell Mark-George
  */
 
 package controllers;
 
-//Nesseccary imports
-import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
+
+import cache.Cache;
 import di.Container;
 import dialogs.DialogStage;
 import javafx.collections.ObservableList;
@@ -34,58 +34,84 @@ import services.interfaces.IPathService;
 import services.interfaces.IResourceLoader;
 import services.interfaces.IStateService;
 
-import cache.Cache;
-
 public class MainController extends Controller {
 
     // JavaFX emelemts loaded from the fxml file
+
+    /** label that displays the current project name */
     @FXML
     private Label projectLabel;
+
+    /** show list of project names */
     @FXML
     private ListView<String> listView;
+
+    /** button used to create new version of current project */
     @FXML
     private Button newVersionButton;
+
+    /** shows versions of current project */
     @FXML
     private TableView<Version> tableView;
+
+    /** column of the tableview */
     @FXML
     private TableColumn<Version, String> versionColumn;
+
+    /** column of the tableview */
     @FXML
     private TableColumn<Version, String> dateColumn;
+
+    /** columns of the tableview */
     @FXML
     private TableColumn<Version, String> commentsColumn;
+
+    /** button to show ProjectDetailsDialog for details of the current project */
     @FXML
     private Button projectDetailsButton;
 
-    //Injecting the needed services from the dependency injection container
+    // Injecting the needed services from the dependency injection container
     IStateService stateService = Container.resolveDependency(IStateService.class);
     IErrorService errorService = Container.resolveDependency(IErrorService.class);
     ICommandService commandService = Container.resolveDependency(ICommandService.class);
     IResourceLoader resouceService = Container.resolveDependency(IResourceLoader.class);
     IPathService pathService = Container.resolveDependency(IPathService.class);
 
-    // Initializable
-
+    // creates an instace of the the cache to store the loaded project
     Cache<Project> cache = new Cache<Project>(5);
     // only 5 projects in the cache at a time
 
+    /**
+     * This method is called when the controller class is created. All inital set up
+     * is done here. 
+     * precondition: the stage is loading and the application itself
+     * is starting (the conrtoller is being loaded) 
+     * 
+     * posetcondition: the stage and the application has finished loading
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        // The table columns are setup to show details from a given Version object
         versionColumn.setCellValueFactory(new PropertyValueFactory<>("versionNumber"));
         dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
         commentsColumn.setCellValueFactory(new PropertyValueFactory<>("comments"));
 
-        // should they be disabled???
-        projectDetailsButton.setDisable(true); //
+        // disables the buttons as there is no selected project on loading
+        // this prevents the attached event listeners from being called
+        projectDetailsButton.setDisable(true);
         newVersionButton.setDisable(true);
 
+        // load the list of projects from the filesystem
         this.loadProjects();
 
+        // Adds event listener for when item in tableview is selected
         tableView.getSelectionModel().selectedItemProperty().addListener((observable, oldVersion, newVersion) -> {
             this.onTableClick(newVersion);
 
         });
 
+        // Adds event listener for when item in listview is selected
         listView.getSelectionModel().selectedItemProperty()
                 .addListener((observable, oldProjectName, newProjectName) -> {
                     this.onListClick(newProjectName);
@@ -104,18 +130,32 @@ public class MainController extends Controller {
 
     }
 
+    /**
+     * This method is responsible for loading the project names from the file
+     * systrem. It updates the state service and the listview with the received
+     * items
+     * 
+     * precondition: the applicationa and stage are still loading postcondition: the
+     * state is updated with the list of project names. If there are any projects, a
+     * palaceholder is set in the listview. Otheriese, the list of project name is
+     * displayed
+     */
     public void loadProjects() {
 
         try {
+            // get the list of project names from the commandService
             ObservableList<String> projects = commandService.getProjects();
-            // stateService.setProjectList(projects);
+            // save the returned result to the stateservice
             stateService.set("projectList", projects);
+            // set the the returned result to the listview
             listView.setItems(projects);
 
+            // if there are no projects, set a placeholder
             if (projects.isEmpty()) {
                 listView.setPlaceholder(new Label("No Projects"));
             }
-        } catch (IOException e) {
+        } catch (Exception e) {
+            // If there is an exception, an errror dialog is shown.
             e.printStackTrace();
             errorService.showErrorDialog("There was an error trying to load projects");
 
@@ -123,25 +163,33 @@ public class MainController extends Controller {
 
     }
 
+    /**
+     * This method is called when an item in the tableview is selected This in turn
+     * updates the state service, and opens the ViewVersion DialogStage, with
+     * information of that version.
+     * 
+     * @param version the version to view
+     * 
+     * precondition: an item in the tableview is selected (there are
+     * items/version in the tableview in the first place) the version
+     * object should be of type Version and can be null (when swiching between projects)
+     *  
+     * postcondition: the version is set in the state service and a ViewVersion dialog should be 
+     * showing with information of that version (filename of the version file, date created, any comments, version number)
+     */
     public void onTableClick(Version version) {
+
         if (version != null) {
             try {
-                System.out.println("Table selected");
-                System.out.println(version);
-
-                // stateService.setCurrentVersion(version);
-
+                // if the version object is not null,
+                // set the given version as the currently select version inthe stateService
                 stateService.set("currentVersion", version);
-
-                // stateService.getViewVersionStage().showAndWait(); // could have used a
-                // custome method here
-
-                Stage viewVersionStage = new DialogStage("View Version", "/resources/fxml/viewversion.fxml", stage); // should
-                                                                                                                     // this
-                                                                                                                     // be
-                                                                                                                     // saved
+                // create a new DialogStage to show the information of the selected version and
+                // show the dialog
+                Stage viewVersionStage = new DialogStage("View Version", "/resources/fxml/viewversion.fxml", stage);
                 viewVersionStage.show();
             } catch (Exception e) {
+                // If there is an exception, an errror dialog is shown.
                 e.printStackTrace();
                 errorService.showErrorDialog("There was a problem viewing the version.");
 
@@ -150,34 +198,54 @@ public class MainController extends Controller {
         }
     }
 
+    /**
+     * This method is called when an item in the listview is selected. This in turn
+     * gets the project with that project name (either from the cache or the
+     * filesystem), and saves it to the stateservice. It also updates some of the UI
+     * elements (like the tableview and projectLabel) with the neccessary data
+     * 
+     * @param projectName the name of the requested project
+     * 
+     *                    precondition: projectName is a string and projectName is
+     *                    the name of an existing project postcondition: the project
+     *                    name and the loaded project assocciated with it are saved
+     *                    in the sate service and the UI is updated with the
+     *                    neccessary information
+     */
     public void onListClick(String projectName) {
 
         try {
+            // if the project is in the cache (meaning it has been selected before)
+            // get it from the cache
             Project selectedProject;
             if (cache.has(projectName)) {
                 System.out.println("in cache");
                 selectedProject = cache.get(projectName);
             } else {
+                // otherwise, load the project from the filesysem and add it to the cache
                 System.out.println("not in cache");
                 selectedProject = resouceService.loadProject(projectName);
                 cache.set(projectName, selectedProject);
             }
 
-            if (projectDetailsButton.isDisabled() || newVersionButton.isDisabled()) { // should it be and
+            // if the projectDetails and newVersion buttons are diabled
+            // (they would be if there is no selected project), enable them
+            if (projectDetailsButton.isDisabled() || newVersionButton.isDisabled()) {
                 projectDetailsButton.setDisable(false);
                 newVersionButton.setDisable(false);
             }
 
-            // stateService.setProjectName(projectName);
+            // update the projectName and current project object in the stateService
             stateService.set("projectName", projectName);
-            // stateService.setCurrentProject(selectedProject);
             stateService.set("currentProject", selectedProject);
 
             projectLabel.setText(projectName);
+            // get the file versions from the project object
             ObservableList<Version> projectVersions = selectedProject.getVersions();
-            // System.out.println(projectVersions);
+            // update the tableView with the list of versions
             tableView.setItems(projectVersions);
         } catch (Exception e) {
+            // If there is an exception, an errror dialog is shown.
             e.printStackTrace();
 
             errorService.showErrorDialog("There was an error in loading the projects.");
@@ -185,49 +253,70 @@ public class MainController extends Controller {
 
     }
 
+    /**
+     * This method is called when the newVersionButton is pressed. It is responsible
+     * for creating a TextInputDialog for any comments (optional) and recording a
+     * new version in the project (reflecting in the UI)
+     * 
+     * precondition: there must be a current project selected postcondition: a new
+     * version of the current project is created and saved to the filesystem
+     */
     @FXML
     public void createNewVersion() {
         // System.out.println("Hello");
 
+        // gets the projectName from the stateService
         String projectName = (String) stateService.get("projectName");
 
         try {
             if (projectName != null) {
+                // if the the returned value is not null,
+                // create a TextInputDialog to enter any comments
                 TextInputDialog dialog = new TextInputDialog();
 
                 dialog.setTitle("Create Version");
                 dialog.setContentText("Enter comments:");
 
+                // show the dialog and get the reseult of the text
                 Optional<String> result = dialog.showAndWait();
-                // System.out.println(result.get());
+                // if the result is present (meaning the the dialog was not dismissed)
                 if (result.isPresent()) {
+                    // get the string result
                     String comments = result.get();
-
+                    // if the result is a balcnk string (has a length of 0 or is just whitespace),
+                    // set the default comment
                     if (comments.isBlank()) {
                         comments = "No Comment";
                     }
 
+                    // replace all whitespace (if any) with underscores
+                    // this is neccessary to be able to parse the version later properly from the
+                    // version file.
                     comments = comments.replaceAll(" ", "_");
 
+                    // create a new version using the given comment
                     Version newVersion = commandService.newVersion(comments);
 
+                    // (should not be null but it is still good to check)
                     if (newVersion != null) {
 
-                        // Project currentProject = stateService.getCurrentProject();
+                        // get the currentProject from the stateService
 
                         Project currentProject = (Project) stateService.get("currentProject");
-
+                        // het the list of versions from the project object
                         ObservableList<Version> projectVersions = currentProject.getVersions();
-
+                        // add the new version to the list of version
+                        // since the list is observable, the tableView will automatically update to show
+                        // this new verion
                         projectVersions.add(newVersion);
 
                     }
 
                 }
-                // System.out.println(result.isPresent());
 
             }
         } catch (Exception e) {
+            // If there is an exception, an errror dialog is shown.
             e.printStackTrace();
 
             errorService.showErrorDialog("There was an error creating a new version of the document.");
@@ -235,37 +324,56 @@ public class MainController extends Controller {
 
     }
 
+    /**
+     * This methd is triggered by the Project menu item. As the name suggests, it
+     * simply creates a new DialogStage to show the NewProject dialog, which
+     * allows users to pick a project name and document path for their new project.
+     * 
+     * precondition: there are no specific condidtions postcondition: the NewProject
+     * dialog is shown
+     */
+    @FXML
     public void showNewProjectDialog() {
-        // Stage newProjectDialog = stateService.getNewProjectStage();
-
-        // Stage newProjectDialog = (Stage) stateService.get("newProjectStage");
 
         try {
+            // creates a new NewProject dialog to show a form for the user to create a new
+            // project and displays the dialog
             Stage newProjectDialog = new DialogStage("Create Project", "/resources/fxml/newproject.fxml", stage);
             newProjectDialog.show();
         } catch (Exception e) {
+            // If there is an exception, an errror dialog is shown.
             e.printStackTrace();
             errorService.showErrorDialog("There was an error in opening the dialog.");// think of better message
         }
 
     }
 
+    /**
+     * This method is triggered when the showProjectDetailsButton is pressed.
+     * This method creates a enw ProjectDetails dialog that diaplays information about the current project
+     * (current version number, latest version number, document path)
+     * 
+     * preconditions: there must be a current project (projectName in state);
+     * postcondition: the ProjectDetails dialog is shown
+     */
     @FXML
     public void showProjectDetails() {
-        // there must be a selsect project to show the details
+        // get the current projectName from the state service 
+    
         String projectName = (String) stateService.get("projectName");
-
+        
         if (projectName != null) {
-            // Stage projectDetailsStage = (Stage) stateService.get("projectDetailsStage");
-            // stateService.getProjectDetailsStage().show();
+            // if the retreived result is not null (meaning there is a project selected)
+            // create a ProjectDetails diolog to show the details of the current project and show it.
             try {
                 Stage projectDetailsStage = new DialogStage("Project Details", "/resources/fxml/projectdetails.fxml",
                         stage);
                 projectDetailsStage.show();
             } catch (Exception e) {
+                // If there is an exception, an errror dialog is shown.
                 e.printStackTrace();
                 errorService.showErrorDialog("There was an error in displaying the project details.");
-                // think of better message
+
             }
 
         }
--- src/controllers/NewProjectController.java
+++ src/controllers/NewProjectController.java
@@ -1,8 +1,19 @@
+/**
+ * The controller class for the NewProject Stage. 
+ * This stage allows the user to create a new project using a project name and the path to the document. 
+ * It implements the Controller interface and its methods.
+ * 
+ * @author Mitchell Mark-George
+ */
+
+// Neccessary imports
 package controllers;
 
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import di.Container;
 import javafx.collections.ObservableList;
@@ -19,31 +30,50 @@ import services.interfaces.IStateService;
 
 public class NewProjectController extends Controller {
 
-    @FXML
-    TextField projectNameTextField;
+    // JavaFX emelemts loaded from the fxml file
 
+    /**test field for project name */
     @FXML
-    Button selectFileButton;
+    private TextField projectNameTextField;
 
+    /** button to trigger filechooser for document path */
     @FXML
-    Button createProjectButton;
+    private Button selectFileButton;
 
+    /** button that creates the bitton */
     @FXML
-    Button closeButton;
+    private Button createProjectButton;
 
+    /** button to close the button */
+    @FXML
+    private Button closeButton;
+
+    /** label that shows the selected document path */
     @FXML
     Label selectedPathLabel;
 
+    /** variable that stores the selected document path for use in other methods */
     private String documentPath;
 
+    
+    // Injecting the needed services from the dependency injection container
     IStateService stateService = Container.resolveDependency(IStateService.class); // cast it to T
     IErrorService errorService = Container.resolveDependency(IErrorService.class);
     IPathService pathService = Container.resolveDependency(IPathService.class);
     ICommandService commandService = Container.resolveDependency(ICommandService.class);
 
+    /**
+     * This method is called when the controller class is created. All inital set up
+     * is done here. 
+     * precondition: the application is running and the main stage is showing
+     * 
+     * posetcondition: the dialog stage has finished loading
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        // the createProjectButton will automatically become disabled whenever 
+        // the projectNameTextField is empty
         createProjectButton.disableProperty().bind(projectNameTextField.textProperty().isEmpty());
 
     }
@@ -60,26 +90,43 @@ public class NewProjectController extends Controller {
 
     }
 
+    /**
+     * This method is responsible for opening a Word Document file chooser and saving the result (displaying it on the UI)
+     *  This method is triggered when the selectFileButton is pressed
+     * 
+     * precondition: 
+     * postcondition: a Word document file is selected and the path to that file is saved
+     */
     @FXML
     public void openFileChooser() {
+        // A new file chooser is created
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Choose Word Document");
+        // the inital directory is set as the home directory (where the user can see all of their folders and files)
         fileChooser.setInitialDirectory(new File(pathService.getHomeDir()));
+        // a filter is used so only Word document files are chosen
         fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Word Documents", "*.doc", "*.docx"));
-        // Stage newProjectStage = (Stage) stateService.get("newProjectStage");
+        // open the file chooser dialog and get the result
         File result = fileChooser.showOpenDialog(stage);
 
         if (result != null) {
+            // if the result is not null,
+            // save the absolute path of the document path in a vaiable
+            // and update the selectedPathLabel element with the absolute path 
             this.documentPath = result.getAbsolutePath();
             System.out.println(documentPath);
             selectedPathLabel.setText(documentPath);
         }
-        //  else {
-        //     errorService.
-        // }
+        
 
     }
 
+    /** This method is triggered by the createProjectButton and is responsible for getting all of the neccerary infomation
+     * (document path, projectName) and create a new project. It also updates the MainStage UI (the listView) with the name of the new project.
+     * 
+     * precondition: the user has filled out the form correctly
+     * postcondition: a new project is created and the dialog is closed
+     */
     @FXML
     public void createProject() {
 
@@ -87,62 +134,64 @@ public class NewProjectController extends Controller {
 
         
 
-
+            // get the project name from the text field
             String projectName = projectNameTextField.getText(); // is required
+            // if any of the needed details are not null or blank (length == 0 or just whitespce)
             if (projectName != null && !projectName.isBlank() && this.documentPath != null) { // even if this is not here, it will still be caught in the Command service
+                // test if there are any illegal characters in the given project name
+                // illegal characters do not do well in the filesystem so this is important
+                // A regex macher is used to test the projectname
+                Pattern pattern = Pattern.compile("[~!&$#@*+%{}<>\\[\\]|\"\\_^]");
+                Matcher matcher = pattern.matcher(projectName);
                 
-                String ILLEGAL_CHARACTERS = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
-
-                if (projectName.contains(ILLEGAL_CHARACTERS)) { // TEST THIS
-                     errorService.showErrorDialog("The project name cannot contain any illegal characters: " + ILLEGAL_CHARACTERS);
+                // if there any illegal characters found, show and error dialog.
+                if (matcher.find()) { // TEST THIS
+                     errorService.showErrorDialog("The project name cannot contain any illegal characters.");
                      return;
                 }
                 
+                // create a new project using the document path and projectName
                 commandService.initProject(this.documentPath, projectName);
-                // ig
-                @SuppressWarnings("unchecked") // casting to this type gives a warning
+                
+                @SuppressWarnings("unchecked") // casting to this type gives a warning in my editor
                 // the state should have some kind of type safety checking
-                ObservableList<String> projectList = (ObservableList<String>) stateService.get("projectList");
-                // stateService.addProject(projectName);
 
+                // get the list of project names from the stateService
+                ObservableList<String> projectList = (ObservableList<String>) stateService.get("projectList");
+              
+                // add the neew project name to the projectList
+                // (because it is observable, it will update in the listview UI element automatically)
                 projectList.add(projectName);
 
-                // ObservableList<String>
-
-                // resetDialog();
-                // Stage newProjectStage = (Stage) stateService.get("newProjectStage");
-                // stateService.getNewProjectStage().close();
-                // newProjectStage.close();
-
+                
+                // close the dialog when done
                 stage.close();
 
             } else {
+                // show trhe error dialog is any of the needed details are invalid
                 errorService.showErrorDialog("No field can be empty. A document path and project name must be chosen.");
             }
         } catch (Exception e) {
+            // If there is an exception, the errror dialog is shown.
             e.printStackTrace();
-            errorService.showErrorDialog(
-                    "There was an error in creating the new project. There might already be a project with the same name,.");
+            errorService.showErrorDialog("There was an error in creating the new project. There might already be a project with the same name,.");
 
         }
 
     }
 
+    /**
+     * This method is triggered by the closeButton and is responsible for closing the NewProject dialog stage
+     * 
+     * precondition: the stage must be open
+     * postcondition: the stage must be closed
+     */
     @FXML
     public void closeStage() {
-        // resetDialog(); // is this still needed here?
-        // // Stage newProjectDialog = stateService.getNewProjectStage();
-
-        // Stage newProjectStage = (Stage) stateService.get("newProjectStage");
-        // newProjectStage.close();
-
+        
         stage.close();
     }
 
-    // private void resetDialog() {
-    // this.documentPath = null;
-    // projectNameTextField.setText(null);
-    // selectedPathLabel.setText("None Selected");
-    // }
+    
 
 }
